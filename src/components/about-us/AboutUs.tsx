ed715eec5519025e6284f85feba8ba89efa27d33
Toochukwu Okolie <charlestoochukwu5@gmail.com>
2022-07-01 17:15:41 +0100

    feat(About-us): implement about-us section mobile and desktop view

--- src/components/about-us/AboutUs.tsx
+++ src/components/about-us/AboutUs.tsx
@@ -1,11 +1,20 @@
 import  "./AboutUs.scss"
+import aboutUsImg from '../../assets/images/about-us.png'
 
 const AboutUs = () => {
 
 
   return (
     <section className="about-us">
-      <h2>About us</h2>
+      <article className="about-us__content">
+        <h2 className="about-us__title">about us</h2>
+        <hr />
+        <p className="about-us__text">She is a product manager who has seen numerous projects from inception to completion. I am presently at WhoGoHost as the product manager</p>
+        <p className="about-us__text">Here are sime of the projects I've overseen so far in my career. Here are sime of the projects I've overseen so far in my career. Here are sime of the projects I've overseen so far in my career. </p>
+      </article>
+      <figure className="about-us__image">
+        <img src={aboutUsImg} alt="about us" />
+      </figure>
     </section>
   )
 }
