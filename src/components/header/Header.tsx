ba78e27f022efbeff6ae4a1282ef7ade987c384d
Toochukwu Okolie <charlestoochukwu5@gmail.com>
2022-07-01 12:30:28 +0100

    feat(header): implement mobile and desktop header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { ArrowUpRight, CheveronDown, HamburgerMenu } from '../../assets/svg/Svg'
 import './Header.scss'
 
 const Header = () => {
   return (
     <header className="header">
-      <h2>header</h2>
+      <nav className="header__nav">
+        <p className="header__logo">denrele <br />paul</p>
+        <ul className="header__navlinks">
+          <li><a href="#">home</a></li>
+          <li><a href="#">about me</a></li>
+          <li><a href="#">projects</a></li>
+          <li>
+            <a href="#">contact me</a>
+            <ArrowUpRight />
+          </li>
+        </ul>
+        <HamburgerMenu />
+      </nav>
+      <article className="header__text">
+        <h1 className="header__text--title">trendy <br/>fashion</h1>
+        <CheveronDown />
+      </article>
     </header>
   )
 }
