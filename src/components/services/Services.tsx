583511a0047f33c9e14aaa80c59dd6a99468d83e
Toochukwu Okolie <charlestoochukwu5@gmail.com>
2022-07-04 20:46:49 +0100

    feat(services): implement mobile-responsive services section

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import './Services.scss'
+import {ReactComponent as ServiceIcon} from '../../assets/images/service.svg'
 
 const Services = () => {
   return (
     <section className="services">
-      <h2>services</h2>
+      <header className="services__header">
+        <h2 className='services__header__title'>services</h2>
+      </header>
+        <hr />
+      <div className="container">
+        <article className="services__service">
+          <ServiceIcon />
+          <h3 className="service__title">web design</h3>
+          <p className="service__details">
+            We help your business achieve outstanding solutions by applying industry standard tools and solutions
+          </p>
+        </article>
+        <article className="services__service">
+           <ServiceIcon />
+          <h3 className="service__title">web design</h3>
+          <p className="service__details">
+            We help your business achieve outstanding solutions by applying industry standard tools and solutions
+          </p>
+        </article>
+        <article className="services__service">
+           <ServiceIcon />
+          <h3 className="service__title">graphics design</h3>
+          <p className="service__details">
+            We help your business achieve outstanding solutions by applying industry standard tools and solutions
+          </p>
+        </article>
+        <article className="services__service">
+           <ServiceIcon />
+          <h3 className="service__title">graphics design</h3>
+          <p className="service__details">
+            We help your business achieve outstanding solutions by applying industry standard tools and solutions
+          </p>
+        </article>
+      </div>
     </section>
   )
 }
