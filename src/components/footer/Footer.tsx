6c9ebc047351559a04fa27c670ecc69662a438f4
Toochukwu Okolie <charlestoochukwu5@gmail.com>
2022-07-07 11:23:07 +0100

    feat(footer): implement mobile responsive footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './Footer.scss'
+import { ReactComponent as FacebookIcon } from '../../assets/images/facebook.svg'
+import { ReactComponent as TwitterIcon } from '../../assets/images/twitter.svg'
+import { ReactComponent as InstagramIcon } from '../../assets/images/instagram.svg'
 const Footer = () => {
   return (
     <footer className="footer">
-      <h2>Footer</h2>
+      <section className="footer__content">
+        <h2 className="footer__content__logo">
+          logo here
+        </h2>
+        <p className="footer__content__text">
+          Live the life you want
+        </p>
+      </section>
+      <section className="footer__socials">
+        <FacebookIcon />
+        <TwitterIcon />
+        <InstagramIcon />
+      </section>
     </footer>
   )
 }
