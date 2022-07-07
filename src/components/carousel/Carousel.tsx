fe7908f14ac9d3a0a582c5d0ab55fbbe5853dbba
Toochukwu Okolie <charlestoochukwu5@gmail.com>
2022-07-07 14:54:54 +0100

    feat: Implement reusable carousel component

---
+++ src/components/carousel/Carousel.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import { ReactComponent as ArrowRight } from '../../assets/images/arrow-right.svg'
+import { ReactComponent as ArrowLeft } from '../../assets/images/arrow-left.svg'
+
+type carouselProps = {
+  slideCount: number,
+  goRight?: boolean,
+  itemsArray: JSX.Element[],
+  autoSlide?: boolean,
+}
+
+const Carousel = ({ slideCount, goRight=false, autoSlide = false, itemsArray }: carouselProps) => {
+
+  const [ currentSlide, setCurrentSlide ] = useState(0);
+  
+  let countDivider: number = 1;
+  console.log(goRight);
+  
+  
+  ((itemsArray?.length % slideCount) == 0 ) ? 
+  (countDivider = itemsArray.length / slideCount) : 
+  (countDivider = Math.floor(itemsArray.length / slideCount) + 1); 
+  
+  useEffect(() => {
+    goRight ? setCurrentSlide(-(countDivider -1)) : setCurrentSlide(0);
+  }, [goRight])
+
+  console.log(currentSlide);
+  
+  // console.log(countDivider);
+
+  const nextSlide = () => {
+    if(goRight){
+      currentSlide < 0 ?
+        setCurrentSlide(currentSlide => currentSlide + 1) :
+          setCurrentSlide(-(countDivider - 1));
+    } else {
+      currentSlide < countDivider - 1 ? 
+        setCurrentSlide(currentSlide => currentSlide + 1) :
+          setCurrentSlide(0);
+    }
+  }
+
+  const prevSlide = () => {
+    if(goRight) {
+      currentSlide > -(countDivider - 1) ?
+        setCurrentSlide(currentSlide => currentSlide - 1) :
+          setCurrentSlide(0);
+    } else {
+      currentSlide > 0 ? 
+        setCurrentSlide(currentSlide => currentSlide - 1) :
+         setCurrentSlide(countDivider - 1); 
+    }
+  }
+
+  return (
+    <section className="carousel">
+      <div className="carousel__container" 
+      style={{transform: `translateX(${
+        goRight ? 
+          (currentSlide / slideCount) * 100 : 
+          -(currentSlide / slideCount) * 100}%)`}}>
+        {itemsArray}
+      </div>
+      <figure className="arrow-right" onClick={nextSlide}>
+        <ArrowRight />
+      </figure>
+      <figure className="arrow-left" onClick={prevSlide}>
+        <ArrowLeft />
+      </figure>
+    </section>
+  )
+}
+
+export default Carousel
